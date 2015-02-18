eef1a40f9a40a74aedd6dbb25f7ff1a9c7709de4
Samuel <samuel@samuel.(none)>
2015-02-18 22:14:54 +0100

    ejercicio tras pasar el pep8

--- aleat.py
+++ aleat.py
@@ -4,12 +4,13 @@
 from webapp import webApp
 import random
 
+
 class Aleat(webApp):
     def process(self, parsedRequest):
-        newURL = str(random.randint(0,10000))
+        newURL = str(random.randint(0, 10000))
         htmlAnswer = "<html><body><p>Hola. <a href=" + newURL + ">Dame otra"
         htmlAnswer = htmlAnswer + "</a></p></body></html>"
         return ("200 OK", htmlAnswer)
 
 if __name__ == '__main__':
-    aleat = Aleat("localhost",9999)        
+    aleat = Aleat("localhost", 9999)
